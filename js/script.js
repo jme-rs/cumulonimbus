2ab5bb3faf6f893d9bd18441cc23b08431348ce0
Hajimen06 <sasaki0hajime@yahoo.co.jp>
2023-01-10 22:43:34 +0900

    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-function butotnClick(){
+function butotnClick() {
     alert('検索は未実装\njavascript test');
 }
 
 document.getElementById('mybtn').addEventListener('click', butotnClick);
+
+function calc(btn) {
+    if (btn.value == "=") {
+        document.calculator.display.value = eval(document.calculator.display.value);
+    } else if (btn.value == "C") {
+        document.calculator.display.value = "";
+    } else {
+        document.calculator.display.value += btn.value;
+    }
+}
