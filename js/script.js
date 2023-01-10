8466316128d8d14f13bd3311874a1df4b85ca06f
Hajimen06 <sasaki0hajime@yahoo.co.jp>
2023-01-11 00:39:27 +0900

    1

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ document.getElementById('mybtn').addEventListener('click', butotnClick);
 function calc(btn) {
     if (btn.value == "C") {
         document.calculator.display.value = "";
-    } else if (btn.value == "←") {
+    }
+    else if (btn.value == "←") {
         document.calculator.display.value = document.calculator.display.value.slice(0, -1);
-    } else if (btn.value == "=") {
+    }
+    else if (btn.value == "=") {
         document.calculator.display.value = eval(document.calculator.display.value);
-    } else {
+    }
+    else {
         document.calculator.display.value += btn.value;
     }
 }
