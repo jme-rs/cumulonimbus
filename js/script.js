634c9c1d2eb85ce604d2c3b02c95e875a3009ec1
Hajimen06 <sasaki0hajime@yahoo.co.jp>
2023-01-11 00:33:09 +0900

    1

--- js/script.js
+++ js/script.js
@@ -6,11 +6,10 @@ document.getElementById('mybtn').addEventListener('click', butotnClick);
 
 function calc(btn) {
     if (btn.value == "C") {
-        document.calculator.display.value = "0";
-    } else if (document.calculator.display.value == "0") {
-        document.calculator.display.value = btn.value;
-    }
-    else if (btn.value == "=") {
+        document.calculator.display.value = "";
+    } else if (btn.value == "←") {
+        document.calculator.display.value = document.calculator.display.value.slice(0, -1);
+    } else if (btn.value == "=") {
         document.calculator.display.value = eval(document.calculator.display.value);
     } else {
         document.calculator.display.value += btn.value;
