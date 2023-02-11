e96399f9d59f5a6e575f5ab26fcbd9216bb1305c
Hajimen06 <sasaki0hajime@yahoo.co.jp>
2023-02-12 00:46:27 +0900

    1

--- js/head.js
+++ js/head.js
@@ -6,7 +6,7 @@ function headLoader() {
     head.insertAdjacentHTML('beforeEnd', '<meta name="theme-color" content="#00B2C6">');
     head.insertAdjacentHTML('beforeEnd', '<meta name="apple-mobile-web-app-capable" content="yes">');
     head.insertAdjacentHTML('beforeEnd', '<meta name="apple-mobile-web-app-status-bar-style" content="default">');
-    head.insertAdjacentHTML('beforeEnd', '<link rel="apple-touch-icon" sizes="640x640" href="/picture/ei-cloud_white.png">');
+    head.insertAdjacentHTML('beforeEnd', '<link rel="apple-touch-icon" sizes="640x640" href="picture/ei-cloud_white.png">');
 
     head.insertAdjacentHTML('beforeEnd', '<meta property="og:type" content="website">');
     head.insertAdjacentHTML('beforeEnd', '<meta property="og:site_name" content="cumulonimbus">');
