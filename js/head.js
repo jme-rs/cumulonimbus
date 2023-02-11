3d0e2428dbc35ccdf4957481e981297d888b0fe8
Hajimen06 <sasaki0hajime@yahoo.co.jp>
2023-02-12 00:23:30 +0900

    a

--- js/head.js
+++ js/head.js
@@ -6,6 +6,12 @@ function headLoader() {
     head.insertAdjacentHTML('beforeEnd', '<meta name="theme-color" content="#00B2C6">');
     head.insertAdjacentHTML('beforeEnd', '<meta name="apple-mobile-web-app-capable" content="yes">');
     head.insertAdjacentHTML('beforeEnd', '<meta name="apple-mobile-web-app-status-bar-style" content="default">');
+    head.insertAdjacentHTML('beforeEnd', '<link rel="apple-touch-icon" sizes="640x640" href="picture/ei-cloud.png">');
+
+    head.insertAdjacentHTML('beforeEnd', '<meta property="og:type" content="website">');
+    head.insertAdjacentHTML('beforeEnd', '<meta property="og:site_name" content="cumulonimbus">');
+    head.insertAdjacentHTML('beforeEnd', '<meta property="og:title" content="@jme.jpが作ったウェブサイト cumulonimbus">');
+    head.insertAdjacentHTML('beforeEnd', '<meta property="og:description" content="今は練習中">');
 
     head.insertAdjacentHTML('beforeEnd', '<link rel="icon" href="picture/ei-cloud.png">');
 
@@ -18,7 +24,7 @@ function headLoader() {
     head.insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" href="css/globalHeader.css">');
     head.insertAdjacentHTML('beforeEnd', '<link rel="stylesheet" href="css/content.css">');
 
-    
+
     console.log("headLoader");
 }
 
